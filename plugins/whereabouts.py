b7a3828b9c687f819d60d13d938db7b13234fc84
snasrallah <simon_nasrallah@hotmail.com>
2018-03-11 15:08:07 +0000

    a bit of formatting

--- plugins/whereabouts.py
+++ plugins/whereabouts.py
@@ -83,5 +83,5 @@ def sync_status(message):
         message.send('No upcoming events found.')
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
-        message.send('-----\n{}\n{}\n{}'.format(start, event['summary'], event['location']))
+        message.send('------\n{}\n{}\n{}'.format(start, event['summary'], event['location']))
    
