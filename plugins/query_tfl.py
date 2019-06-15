c52e56d6040f7a3ecb323b087546c526a5aaa49e
nasras03 <simon.nasrallah@bbc.co.uk>
2019-06-15 12:12:10 +0100

    take care of messy config

--- plugins/query_tfl.py
+++ plugins/query_tfl.py
@@ -12,7 +12,7 @@ tfl_query.load_properties('/home/pi/slackbot/mybot/scripts/tfl_api_key.config')
 
 @listen_to('^\.bus$', re.IGNORECASE)
 def tfl_check(message):
-    message.send('locations: {}'.format(tfl_query.locations.keys()))
+    message.send('locations: {}'.format(tfl_query.LOCATIONS.keys()))
 
 
 @listen_to('^\.bus (.*)', re.IGNORECASE)
@@ -21,7 +21,7 @@ def tfl_check(message, stop_id='0'):
     if type(buses_ordered_by_time) == str:
         message.send(buses_ordered_by_time)
     else:
-        message.send('{} Arrivals'.format(tfl_query.locations.get(stop_id, 'no result').split()[1]))
+        message.send('{} Arrivals'.format(tfl_query.LOCATIONS.get(stop_id, 'no result').split()[1]))
         for count, (name_dest, arrival) in enumerate(buses_ordered_by_time.iteritems(), 1):
             message.send('{}. {} {} [{}]'.format(count, name_dest[0], name_dest[1],
                                                  str(datetime.timedelta(seconds=arrival))))
